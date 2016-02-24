95a5c79b47afe0ed9b37d5b0b015ee1bdd62883d
Nicolai Reigstad <nicolai887@gmail.com>
2016-02-24 10:22:54 +0100

    Initial commit

---
+++ Project Manager/resources/javascript/BacklogItem.js
@@ -0,0 +1,9 @@
+var BacklogItem = function (id, title, estimat, priority, creator, responseble) {
+    this.id = id;
+    this.title = title;
+    this.estimat = estimat;
+    this.priority = priority;
+    this.creator = creator;
+    this.responseble = responseble;
+    this.createdAt;
+};
---
+++ Project Manager/resources/javascript/Project.js
@@ -0,0 +1,7 @@
+var Project = function(id,image, name, userList, creator, settings){
+    this.id = id;
+    this.image = image;
+    this.name = name;
+    this.userList = userList;
+    this.creator = creator;
+};
