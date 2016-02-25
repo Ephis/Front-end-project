ca520f45d1b8027a2158eac3d5ca337b261304d6
Nicolai Reigstad <nicolai887@gmail.com>
2016-02-25 12:59:38 +0100

    Models refactored

---
+++ Project Manager - Begyndelse HTML/resources/javascript/User.js
@@ -0,0 +1,8 @@
+(var User = function(id, userName, name, pictureUrl, profileText, projects){
+    this.id = id;
+    this.userName = userName;
+    this.name = name;
+    this.pictureUrl = pictureUrl;
+    this.profileText = profileText;
+    this.projects = projects;
+};)
