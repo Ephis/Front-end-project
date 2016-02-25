df45760ad664749eb6a17a941817e5fa61d40433
Marck Jensen <marckjensen93@hotmail.com>
2016-02-25 12:51:23 +0100

    Created project model: Task

---
+++ Project Manager - Begyndelse HTML/resources/javascript/Task.js
@@ -0,0 +1,10 @@
+(var Task = function(){
+    this.Id = Id;
+    this.name = name;
+    this.description = description;
+    this.estimate = estimate;
+    this.stauts = status;
+    this.responseable = responseable;
+    this.Story = Story;
+    this.createdAt = Date.now();
+};)
