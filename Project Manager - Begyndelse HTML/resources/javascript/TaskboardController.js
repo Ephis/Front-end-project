3859a9a4d2077fc7daf5952e0c641d972c754a57
Marck Jensen <marckjensen93@hotmail.com>
2016-03-08 13:08:58 +0100

    Added list of Tasks in ng-view: taskboard

--- Project Manager - Begyndelse HTML/resources/javascript/TaskboardController.js
+++ Project Manager - Begyndelse HTML/resources/javascript/TaskboardController.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp');
 
 app.controller('TaskboardController', function () {
-
+    var self = this; 
+    self.tasks = Tasks;
     
 });
+
+var Tasks = [new Task(1,"Task 1","desc",2,"","uswr1","story")];
