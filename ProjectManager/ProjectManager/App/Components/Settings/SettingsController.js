bf24c8efb66f2887de90dbbb9967594067bef1d6
Marck Jensen <marckjensen93@hotmail.com>
2016-03-17 16:58:55 +0100

    added settings view

---
+++ ProjectManager/ProjectManager/App/Components/Settings/SettingsController.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('maApp');
+
+app.controller('SettingsController', function() {
+    
+    window.onload = function () {
+        var videoPlayer = document.querySelector('.video-container > video');
+        var videoPlayPause = document.querySelector('#play');
+        var newVideoPlayer = new VideoPlayer(videoPlayer, VideoPlayPause);
+
+        videoPlayPause.addEventListener('click', function (evt) {
+            newVideoPlayer.playVideo();
+        });
+    }
+
+    var VideoPlayer = function (videoElement, btnPlayPause) {
+        this.videoElement = videoElement;
+        this.btnPlayPause = btnPlayPause;
+    }
+
+    //VideoPlayer play & pause
+
+    VideoPlayer.prototype.togglePlay = function () {
+        if (this.videoElement.paused) {
+            this.videoElement.play();
+            this.btnPlayPause.style.backgroundColor = "#00FF00";
+        } else {
+            this.videoElement.pause();
+            this.btnPlayPause.style.backgroundColor = "#FF0000";
+        }
+    }
+});
