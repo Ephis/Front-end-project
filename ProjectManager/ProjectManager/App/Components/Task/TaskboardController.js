11441e0fc31db5023d412f907b89db2d1e4e21f5
Marck Jensen <marckjensen93@hotmail.com>
2016-03-17 13:06:24 +0100

    More validtion in story

--- ProjectManager/ProjectManager/App/Components/Task/TaskboardController.js
+++ ProjectManager/ProjectManager/App/Components/Task/TaskboardController.js
@@ -1,9 +1,18 @@
 var app = angular.module('myApp');
 
-app.controller('TaskboardController',['$scope', function ($scope) {
+app.controller('TaskboardController',['$scope' ,function ($scope) {
     var self = this; 
     self.tasks = Tasks;
+    self.task = [];
+
+    self.addTask = function () {
+        var task = $scope.task;
+        $scope.task = null;
+        $scope.models.lists.ToDo.push(task);
+    };
     
+    
+
     $scope.stories = Stories;
 
     $scope.models = {
@@ -11,16 +20,20 @@ app.controller('TaskboardController',['$scope', function ($scope) {
         lists: { "ToDo": [], "In progress": [], "Review": [], "Done": [] }
     };
 
+    
     // Generate initial model
-    for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.ToDo.push({ label: "Item A" + i });
-        $scope.models.lists["In progress"].push({ label: "Item B" + i });
-    };
+    //for (var i = 1; i <= 3; ++i) {
+      //  $scope.models.lists.ToDo.push({ label: "Item A" + i });
+        //$scope.models.lists["In progress"].push({ label: "Item B" + i });
+    //};
+    
 
     // Model to JSON for demo purpose
     $scope.$watch('models', function (model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
+
+
 
 }]);
 
