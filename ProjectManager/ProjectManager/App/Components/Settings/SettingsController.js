2c771d64160fb47256b8c392b29fb7941dedcc04
Marck Jensen <marckjensen93@hotmail.com>
2016-03-17 20:10:09 +0100

    tried to add geolocation and video

--- ProjectManager/ProjectManager/App/Components/Settings/SettingsController.js
+++ ProjectManager/ProjectManager/App/Components/Settings/SettingsController.js
@@ -1,11 +1,11 @@
-﻿var app = angular.module('maApp');
+﻿var app = angular.module('myApp');
 
 app.controller('SettingsController', function() {
     
     window.onload = function () {
         var videoPlayer = document.querySelector('.video-container > video');
         var videoPlayPause = document.querySelector('#play');
-        var newVideoPlayer = new VideoPlayer(videoPlayer, VideoPlayPause);
+        var newVideoPlayer = new VideoPlayer(videoPlayer, videoPlayPause);
 
         videoPlayPause.addEventListener('click', function (evt) {
             newVideoPlayer.playVideo();
@@ -19,13 +19,50 @@ app.controller('SettingsController', function() {
 
     //VideoPlayer play & pause
 
-    VideoPlayer.prototype.togglePlay = function () {
+    VideoPlayer.prototype.playVideo = function () {
         if (this.videoElement.paused) {
             this.videoElement.play();
             this.btnPlayPause.style.backgroundColor = "#00FF00";
         } else {
             this.videoElement.pause();
             this.btnPlayPause.style.backgroundColor = "#FF0000";
+        }
+    }
+
+    //Geolocation
+
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+        var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+        + latlon + "&zoom=14&size=400x300&sensor=false";
+        document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "An unknown error occurred."
+                break;
         }
     }
 });
